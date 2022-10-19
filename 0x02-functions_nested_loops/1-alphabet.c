d75456e20bc5300807582b6401bc55d1ff586745
Interferons <cynthiachukwuemeka119@yahoo.com>
2022-10-19 12:35:56 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,26 @@
  * Return: Always 0.
  */
 
+
+
 void print_alphabet(void)
 
+
+
 {
+
 	char alphabet;
 
+
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
 	{
+
 		_putchar(alphabet);
+
 	}
+
 	_putchar('\n');
+
 }

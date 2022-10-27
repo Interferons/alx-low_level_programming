03bed2f97cc7a3e9727bf3c5400685b2df8a5f30
Interferons <cynthiachukwuemeka119@yahoo.com>
2022-10-27 15:13:01 -0700

    Yasl 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,30 +1,27 @@
+
 #include "main.h"
 /**
- * cap_string - capitalizes all words of a string
- * @s: input string.
+ * _strcat - concatenates two strings,
+ * @dest: destination.
+ * @src: source.
  * Return: the pointer to dest.
  */
-
-char *cap_string(char *s)
+char *_strcat(char *dest, char *src)
 {
-	int count = 0, i;
-	int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int count = 0, count2 = 0;
 
-	if (*(s + count) >= 97 && *(s + count) <= 122)
-		*(s + count) = *(s + count) - 32;
-	count++;
-	while (*(s + count) != '\0')
+	while (*(dest + count) != '\0')
 	{
-		for (i = 0; i < 13; i++)
-		{
-			if (*(s + count) == sep_words[i])
-			{
-				if ((*(s + (count + 1)) >= 97) && (*(s + (count + 1)) <= 122))
-					*(s + (count + 1)) = *(s + (count + 1)) - 32;
-				break;
-			}
-		}
 		count++;
 	}
-	return (s);
-}}
+
+	while (count2 >= 0)
+	{
+		*(dest + count) = *(src + count2);
+		if (*(src + count2) == '\0')
+			break;
+		count++;
+		count2++;
+	}
+	return (dest);
+}
